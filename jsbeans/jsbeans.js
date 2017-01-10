ca7452c415ac37632f2b6b7419434ad514eb0333
Aleksander Gajewski <adiog@brainfuck.pl>
2017-01-10 09:15:36 +0100

    ecmascript

--- jsbeans/jsbeans.js
+++ jsbeans/jsbeans.js
@@ -1,40 +1,31 @@
 // This file is a part of pybeans project.
 // Copyright (c) 2017 Aleksander Gajewski <adiog@brainfuck.pl>.
 
+
 if (!Array.prototype.last){
     Array.prototype.last = function(){
         return this[this.length - 1];
     };
 };
 
+
 var isinstanceof =
 function(an_object, a_typoid)
 {
-    if (a_typoid.__class__)
+    if (Typoid.prototype.isPrototypeOf(a_typoid))
     {
-        if (a_typoid.__class__.indexOf("Typoid") != -1)
-        {
-            return a_typoid.is_instance_of(an_object);
-        }
-        else if (a_typoid.__class__.indexOf("Bean") != -1)
-        {
-            return a_typoid.is_instance_of(an_object);
-        }
+        return a_typoid.is_instance_of(an_object);
+    }
+    else if (Bean.prototype.isPrototypeOf(a_typoid))
+    {
+        return a_typoid.is_instance_of(an_object);
     }
     else
     {
-
+        throw "RuntimeError()";
     }
 };
 
-var issubclass =
-function(an_object, a_class)
-{
-    ret = (an_object.__class__) &&
-           (a_class.__class__) &&
-           (an_object.__class__.indexOf(a_class.__class__.last()) != -1);
-    return ret;
-};
 
 var Atom =
 function(variant)
@@ -44,13 +35,17 @@ function(variant)
     this.__call__ =
     function(single_arg)
     {
-        if (issubclass(this.variant, class_Typoid))
+        if (Typoid.prototype.isPrototypeOf(this.variant))
         {
             return this.variant.__call__(single_arg);
         }
-        else if (issubclass(this.variant.prototype, class_Bean))
+        else //if (Bean.prototype.isPrototypeOf(this.variant))
+             if (Bean.prototype.isPrototypeOf(this.variant) || ('Bean' == this.variant.__proto__.name))
         {
-            return new this.variant(single_arg);
+            console.log(this.variant);
+            let a = new (this.variant)(single_arg);
+            Bean.prototype.isPrototypeOf(a);
+            return a;
         }
         else
         {
@@ -73,13 +68,14 @@ function(variant)
     this.is_optional =
     function()
     {
-        return (this.variant.__class__.indexOf('Optional') != -1);
+        return (Optional.prototype.isPrototypeOf(this.variant));//.__class__.indexOf('Optional') != -1);
     };
 
     this.is_default =
     function()
     {
-        return (this.variant.__class__.indexOf('Default') != -1);
+        return (Default.prototype.isPrototypeOf(this.variant));//.__class__.indexOf('Optional') != -1);
+        //return (this.variant.__class__.indexOf('Default') != -1);
     };
 };
 
@@ -126,10 +122,14 @@ var Register =
     {
         if (type_list.length == 1)
         {
-            atom_class = this.atoms[type_list[0]].prototype.__class__;
-            if (atom_class.indexOf("Bean") != -1)
+            atom = this.atoms[type_list[0]];
+            //if (atom instanceof Bean)
+            console.log('o');
+            console.log(atom);
+            if (Bean.prototype.isPrototypeOf(atom) || ('Bean' == atom.__proto__.name))
             {
-                return this.atoms[type_list[0]];
+                console.log('x');
+                return atom; //this.atoms[type_list[0]];
             }
             else
             {
@@ -153,92 +153,55 @@ var Register =
 
 
 var register_atom =
-function(atom_name, atom_prototype)
+function(atom_name, atom_class)
 {
-    atom_constructor = function(arg){atom_prototype.__init__(this, arg)};
-    atom_constructor.prototype = atom_prototype;
-    Register.atoms[atom_name] = atom_constructor;
+    Register.atoms[atom_name] = atom_class;
 };
 
 
 var register_bean_spec =
-function(bean_name, bean_prototype, bean_spec)
+function(bean_name, bean_class, bean_spec)
 {
-    bean_prototype.__name__ = bean_name;
-    bean_prototype.__class__.last = bean_name;
-    register_atom(bean_name, bean_prototype);
+    register_atom(bean_name, bean_class);
     bean_json = JSON.parse(bean_spec);
     bean_dict = {}
     for (k in bean_json)
     {
         bean_dict[k] = Register.get(bean_json[k]);
     }
-    Register.specs[bean_name] = bean_dict;
+    Register.specs[bean_class.name] = bean_dict;
 };
 
 
-var public =
-function(base_prototype, derived_prototype)
+class Typoid
 {
-    var public_prototype = Object.assign(Object.assign({}, base_prototype), derived_prototype);
-    public_prototype.__class__ = base_prototype.__class__.concat(derived_prototype.__class__);
-    return public_prototype;
-};
+    constructor() {}
 
-var class_Typoid =
-{
-    __class__: ["Typoid"],
-
-    __init__:
-    function(that)
-    {
-    },
-
-    __call__:
-    function(simple_data)
-    {
-        alert('NotImplementedError()');
-    },
-
-    is_instance_of:
-    function(self, an_object)
-    {
-        alert('NotImplementedError()');
-    },
-
-    to_simple_data:
-    function(self, an_object)
+    __call__(simple_data)
     {
         alert('NotImplementedError()');
     }
-};
 
-
-var class_BaseType = public(class_Typoid,
-{
-    __class__: ["BaseType"],
-
-    is_instance_of:
-    function(an_object)
+    is_instance_of(an_object)
     {
         alert('NotImplementedError()');
-    },
+    }
 
-    __call__:
-    function(an_object)
+    to_simple_data(an_object)
     {
-        if (this.is_instance_of(an_object))
-        {
-            return an_object;
-        }
-        else
-        {
-            throw 'TypeError()';
-        }
-    },
+        alert('NotImplementedError()');
+    }
+}
 
-    to_simple_data:
-    function(an_object)
+
+class BaseType extends Typoid
+{
+    is_instance_offunction(an_object)
+    {
+        alert('NotImplementedError()');
+    }
+
+    __call__(an_object)
     {
         if (this.is_instance_of(an_object))
         {
@@ -249,47 +212,49 @@ var class_BaseType = public(class_Typoid,
             throw 'TypeError()';
         }
     }
-});
 
+    to_simple_data(an_object)
+    {
+        if (this.is_instance_of(an_object))
+        {
+            return an_object;
+        }
+        else
+        {
+            throw 'TypeError()';
+        }
+    }
+}
 
-var class_Int = public(class_BaseType,
+class Int extends BaseType
 {
-    __class__: ["Int"],
-
-    is_instance_of:
-    function(an_object)
+    is_instance_of(an_object)
     {
         return Object.prototype.toString.call(an_object) === "[object Number]";
     }
-});
-register_atom("Int", class_Int);
+}
+register_atom("Int", Int);
 
 
-var class_String = public(class_BaseType,
+class String extends BaseType
 {
-    __class__: ["String"],
-
-    is_instance_of:
-    function(an_object)
+    is_instance_of(an_object)
     {
         return Object.prototype.toString.call(an_object) === "[object String]";
     }
-});
-register_atom("String", class_String);
+}
+register_atom("String", String);
 
 
-var class_Optional = public(class_Typoid,
+class Optional extends Typoid
 {
-    __class__: ["Optional"],
-
-    __init__:
-    function(that, element_type)
+    constructor(element_type)
     {
-        that.element_type = element_type;
-    },
+        super();
+        this.element_type = element_type;
+    }
 
-    __call__:
-    function(an_optional)
+    __call__(an_optional)
     {
         if (an_optional == null)
         {
@@ -299,16 +264,14 @@ var class_Optional = public(class_Typoid,
         {
             return this.element_type.__call__(an_optional);
         }
-    },
+    }
 
-    is_instance_of:
-    function(an_object)
+    is_instance_of(an_object)
     {
         return ((an_object == null) || (isinstanceof(an_object, this.element_type)));
-    },
+    }
 
-    to_simple_data:
-    function(an_object)
+    to_simple_data(an_object)
     {
         if (an_object == null)
         {
@@ -319,79 +282,69 @@ var class_Optional = public(class_Typoid,
             return this.element_type.to_simple_data(an_object);
         }
     }
-});
-register_atom("Optional", class_Optional);
+}
+register_atom("Optional", Optional);
 
 
-var class_Forward = public(class_Typoid,
+class Forward extends Typoid
 {
-    __class__: ["Forward"],
-
-    __init__:
-    function(that, element_type)
+    constructor(element_type)
     {
-        that.element_type = element_type;
-    },
+        super();
+        this.element_type = element_type;
+    }
 
-    __call__:
-    function(an_optional)
+    __call__(an_optional)
     {
         return this.element_type.__call__(an_optional);
-    },
+    }
 
-    is_instance_of:
-    function(an_object)
+    is_instance_of(an_object)
     {
         return isinstanceof(an_object, this.element_type);
-    },
+    }
 
-    to_simple_data:
-    function(an_object)
+    to_simple_data(an_object)
     {
         return this.element_type.to_simple_data(an_object);
     }
-});
-register_atom("Optional", class_Optional);
+}
+register_atom("Optional", Optional);
 
 
-var class_Default = public(class_Forward, {
-    __class__: ["Default"]
-});
-register_atom("Default", class_Default);
-
-
-var class_List = public(class_Typoid,
+class Default extends Forward
 {
-    __class__: ["List"],
+}
+register_atom("Default", Default);
 
-    __init__:
-    function(that, element_type)
-    {
-        that.element_type = element_type;
-    },
 
-    __call__:
-    function(a_list)
+class List extends Typoid
+{
+    constructor(element_type)
     {
-        that = this;
+        super();
+        this.element_type = element_type;
+    }
+
+    __call__(a_list)
+    {
+        let that = this;
         return a_list.map(function(an_element){return that.element_type.__call__(an_element);});
-    },
+    }
 
-    is_instance_of:
-    function(a_list)
+    is_instance_of(a_list)
     {
-        than = this;
+        let than = this;
         return a_list.every(function(an_element){return isinstanceof(an_element, that.element_type);});
-    },
+    }
 
-    to_simple_data:
-    function(a_list)
+    to_simple_data(a_list)
     {
-        that = this;
+        let that = this;
         return a_list.map(function(an_element){return that.element_type.to_simple_data(an_element);});
     }
-});
-register_atom("List", class_List);
+}
+register_atom("List", List);
 
 /*
 class DateTime(Typoid):
@@ -414,28 +367,24 @@ class DateTime(Typoid):
 
 
 
-var class_Bean =
+class Bean
 {
-    __class__: ['Bean'],
-
-    get_spec:
-    function()
+    get_spec()
     {
-        return Register.specs[this.__name__];
-    },
+        console.log(this.constructor.name);
+        return Register.specs[this.constructor.name];
+    }
 
-    in_spec:
-    function(simple_data)
+    in_spec(simple_data)
     {
         return true;
         // FIXME TODO SEND NUDES
         // return set(simple_data.keys()).issubset(cls.get_spec().keys());
-    },
+    }
 
-    is_instance_of:
-    function(cls, an_object)
+    is_instance_of(cls, an_object)
     {
-        spec = this.get_spec();
+        let spec = this.get_spec();
 
         if (!this.in_spec(an_object))
         {
@@ -450,32 +399,37 @@ var class_Bean =
         }
 
         return true;
-    },
+    }
 
-    to_simple_data:
-    function(an_object)
+    to_simple_data(an_object)
     {
-        spec = this.get_spec();
-        simple_data = {};
+        let spec = this.get_spec();
+        let simple_data = {};
         for(k in spec)
         {
             simple_data[k] = spec[k].to_simple_data(this.k);
         }
-    },
+        return simple_data;
+    }
 
-    cast_spec_dict:
-    function(simple_data)
+    cast_spec_dict(simple_data)
     {
-        spec = this.get_spec();
+        var spec = this.get_spec();
         for(k in spec)
         {
-            this[k] = this.cast_spec_field(k, spec[k], simple_data);
+            this.cast_spec_dict_assign_field(k, spec[k], simple_data);
+            console.log(k);
         }
-    },
+    }
 
-    cast_spec_field:
-    function(field, field_spec, simple_data)
+    cast_spec_dict_assign_field(field, field_spec, simple_data)
     {
+        this[k] = this.cast_spec_field(field, field_spec, simple_data);
+    }
+
+    cast_spec_field(field, field_spec, simple_data)
+    {
+        console.log(simple_data);
         if (field in simple_data)
         {
             return field_spec.__call__(simple_data[field]);
@@ -495,30 +449,40 @@ var class_Bean =
                 throw "TypeError()";
             }
         }
-    },
+    }
 
-    get_default:
-    function(field)
+    get_default(field)
     {
         throw "NotImplementedError()";
-    },
+    }
 
-    __init__:
-    function(that, simple_data)
+    constructor(simple_data)
     {
-        if (that.in_spec(simple_data))
+        console.log(simple_data);
+        if (this.in_spec(simple_data))
         {
-            that.cast_spec_dict(simple_data);
+            this.cast_spec_dict(simple_data);
         }
         else
         {
             throw "TypeError()";
         }
-    },
+        console.log(this);
+    }
 
-    get_id:
-    function()
+    get_id()
     {
         return this[this.__name__ + '_id'];
     }
-};
+}
+
+class MinimalBean extends Bean
+{
+    cast_spec_dict_assign_field(field, field_spec, simple_data)
+    {
+        if ((!field_spec.is_default()) || (field in simple_data))
+        {
+            this[k] = this.cast_spec_field(k, spec[k], simple_data);
+        }
+    }
+}
